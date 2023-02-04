e3a2a9e84cfae080c6475d0d4a86943e8f148659
manavm0 <s0mumana@uni-bonn.de>
2023-02-04 12:41:01 +0100

    changes in test_NER

--- project_package/tests/test_NER.py
+++ project_package/tests/test_NER.py
@@ -3,18 +3,28 @@
 # from pathlib import Path
 # import spacy
 from mapd.NER import EntityPrediction
+from sqlalchemy import select, inspect,create_engine
+from sqlalchemy.orm import Session
+import unittest
 
+DB_PATH='project_package/tests/data/Test_DB.db'
+CONN_STRING = f"sqlite:///{DB_PATH}"
+engine = create_engine(CONN_STRING)
+test_session = Session(bind=engine)
 
-class TestModel:
+class TestModel(unittest.TestCase):
     """Unit tests to check model prediction"""
 
     def test_entity_prediction(self):
         """Checks if the model prediction is as expected"""
         text = "Merkel cell polyomavirus (MCPyV) is the only human polyomavirus currently known to cause human cancer."
-        expected_output = [("Merkel cell polyomavirus", "CELL"), ("human polyomavirus", "ORGANISM"), ("human cancer", "ORGANISM")]
+        # expected_output = [("Merkel cell polyomavirus", "CELL"), ("human polyomavirus", "ORGANISM"), ("human cancer", "ORGANISM")]
+        expected_output=[{'entity': 'Merkel cell polyomavirus', 'labels': 'CELL'},
+                         {'entity': 'human polyomavirus', 'labels': 'ORGANISM'},
+                         {'entity': 'human cancer', 'labels': 'ORGANISM'}]
         message = "Predicted entities and expected output does not match"
 
-        test_model = EntityPrediction()
+        test_model = EntityPrediction(test_session)
         entities = test_model.predict_entities(text)
 
         self.assertEqual(entities, expected_output, message)
@@ -23,7 +33,7 @@ class TestModel:
         """Check model prediction for an empty string"""
         text = ""
         expected_output = []
-        test_model = EntityPrediction()
+        test_model = EntityPrediction(test_session)
         entities = test_model.predict_entities(text)
         message = "Predicted entities and expected output does not match"
 
