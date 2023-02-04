85759bccb0cda0be64c396d41d805ecf9bdee078
Parinishtha <parinishtha.bhalla@gmail.com>
2023-02-04 11:40:43 +0100

    NER unit tests

--- project_package/tests/test_NER.py
+++ project_package/tests/test_NER.py
@@ -1,1 +1,30 @@
 """NER module tests."""
+
+# from pathlib import Path
+# import spacy
+from mapd.NER import EntityPrediction
+
+
+class TestModel:
+    """Unit tests to check model prediction"""
+
+    def test_entity_prediction(self):
+        """Checks if the model prediction is as expected"""
+        text = "Merkel cell polyomavirus (MCPyV) is the only human polyomavirus currently known to cause human cancer."
+        expected_output = [("Merkel cell polyomavirus", "CELL"), ("human polyomavirus", "ORGANISM"), ("human cancer", "ORGANISM")]
+        message = "Predicted entities and expected output does not match"
+
+        test_model = EntityPrediction()
+        entities = test_model.predict_entities(text)
+
+        self.assertEqual(entities, expected_output, message)
+
+    def test_entity_prediction_empty_text(self):
+        """Check model prediction for an empty string"""
+        text = ""
+        expected_output = []
+        test_model = EntityPrediction()
+        entities = test_model.predict_entities(text)
+        message = "Predicted entities and expected output does not match"
+
+        self.assertEqual(entities, expected_output, message)
