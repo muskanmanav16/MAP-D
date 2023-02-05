df377f365280fda4f266b16d29979c99239b8f68
Deepika Pradeep <s0deprad@uni-bonn.de>
2023-02-05 18:40:15 +0100

    query-db edit

--- project_package/mapd/cli.py
+++ project_package/mapd/cli.py
@@ -62,7 +62,7 @@ def get_entity_dict(row_wise_results: bool):
 
 @main.command()
 @click.argument('pmid')
-def get_abstract_info(pmid): # can test with PMID 36316711
+def get_abstract_info(pmid: int): # can test with PMID 36316711
     """Retrieves information about an abstract given its PMID (PubMed ID).
 
     Parameters
@@ -76,7 +76,7 @@ def get_abstract_info(pmid): # can test with PMID 36316711
 
 @main.command()
 @click.argument('text')
-def predict_entities(text):
+def predict_entities(text: str):
     """Given a text, predicts entities using Scispacy NER model.
     Parameters
     ----------
@@ -93,21 +93,21 @@ def predict_entities(text):
 @main.command()
 @click.argument('keyword')
 @click.argument('filepath')
-@click.option('s', '--start_date', default=None, help='start date for time range of desired query result')
-@click.option('e', '--end_date', default=None, help='end date for time range of desired query result')
-def query_db(keyword: str, filepath, start_date=None, end_date=None):
+@click.option('-s', '--start_date', default=None, help='start date for time range of desired query result')
+@click.option('-e', '--end_date', default=None, help='end date for time range of desired query result')
+def query_db(keyword: str, filepath: str, start_date=None, end_date=None):
     """Queries database for keyword and (optionally) date range, and saves results to file at specified address.
     Parameters
     ----------
     keyword: str
         Keyword to query database with
     filepath: str
-        File path for query results file
+        File path for query results .csv file
     """
 
     results = query_database(keyword, start_date, end_date)
     for result in results:
-        result['id'] = f'=HYPERLINK("https://www.ncbi.nlm.nih.gov/pubmed/{result["id"]}","{result["id"]}")'
+        # result['id'] = f'=HYPERLINK("https://www.ncbi.nlm.nih.gov/pubmed/{result["id"]}","{result["id"]}")'
         result['abstract_text'] = result['abstract_text'].replace('<mark>' + keyword + '</mark>', keyword)
 
     df = pd.DataFrame(results)
@@ -116,14 +116,13 @@ def query_db(keyword: str, filepath, start_date=None, end_date=None):
     timerange = ' '
     if start_date and end_date:
         timerange = 'in time range ' + start_date + ' and ' + end_date
-
-    click.echo('Results file for query {}{} stored at {}'.format(keyword, timerange, filepath))
+    click.echo('Results file for query {} {} stored at {}'.format(keyword, timerange, filepath))
 
 
 @main.command()
 def serve():
     """Starts web server."""
-    uvicorn.run("frontend.Frontend_progress.run:app")  # this may need editing
+    uvicorn.run("frontend.Frontend_progress.run:app")  # this needs editing
 
 #
 if __name__ == "__main__":
