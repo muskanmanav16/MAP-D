0c01b18be55a1c22539570bf40a1c32c3f19f0d0
Deepika Pradeep <s0deprad@uni-bonn.de>
2023-02-05 16:26:10 +0100

    build db command

--- project_package/mapd/cli.py
+++ project_package/mapd/cli.py
@@ -12,16 +12,19 @@ logger.setLevel(logging.DEBUG)
 # Instantiate Database class, add abstracts from cached/freshly downloaded files if they do not already exist:
 db = Database()
 db.add_abstract_to_database()
-if not db.raw_entity_data: # if entities have not already been predicted
-    db.get_entity_dict()
 
 # Creating Click group:
 @click.group()
 def main():
-
     """Entry method"""
     pass
 
+@main.command()
+def build_db:
+    """Builds database and populates it with abstract records using either cached/newly downloaded files."""
+    db = Database()
+    db.add_abstract_to_database()
+    click.echo("Built database of PubMed abstracts at {}.".format(DB_PATH))
 @main.command()
 def rebuild_db():
     """Rebuilds database from scratch after dropping any existing tables, and adds associated entities to Entity table"""
@@ -29,13 +32,11 @@ def rebuild_db():
     db.rebuild_database()
     db.add_abstract_to_database()
     db.add_entity_data()
-    click.echo(
-        """Rebuilt database of PubMed abstracts from scratch at {}.
-        Entity table of database populated with entities for each abstract.""".format(DB_PATH))
+    click.echo("Rebuilt database of PubMed abstracts from scratch at {}.".format(DB_PATH))
+    click.echo("Entity table of database populated with entities for each abstract.")
 
 @main.command()
-@click.option('-r', '--row_wise_results', is_flag=True, default=False,
-              help='option to print dictionary entries {entity:label} row by row')
+@click.option('-r', '--row_wise_results', default=False, is_flag=True)#, help="option to print dict entries row by row")
 def get_entity_dict(row_wise_results):
     """Returns a dictionary of entities in the table, with each entry containing an entity (key) and its label (value)"""
 
@@ -67,7 +68,7 @@ def get_abstract_info(pmid):
 
 
 @main.command()
-@click.argument('abstract_id', help='ID of the associated abstract in the Abstract table of the database')
+@click.argument('abstract_id')
 @click.argument('entities')
 def insert_entities(abstract_id, entities):
     """Adds entities into the Entity table in the database for a given.
@@ -131,8 +132,8 @@ def query_db(keyword: str, filepath, start_date=None, end_date=None):
 
 @main.command()
 def serve():
-    """Start the API web server."""
-    uvicorn.run("group2.frontend.Frontend_progress.run:app") # this may need editing
+    """Starts web server."""
+    uvicorn.run("frontend.Frontend_progress.run:app") # this may need editing
 
 if __name__ == "__main__":
     main()
