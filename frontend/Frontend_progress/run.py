1b00483a71dc8294647df0e6c95d5bc45fff86a6
ashta <asthaanand091995@gmail.com>
2023-02-05 02:42:27 +0100

    compiled tasks

--- frontend/Frontend_progress/run.py
+++ frontend/Frontend_progress/run.py
@@ -3,23 +3,23 @@ import os
 
 from flask import Flask, render_template, request, make_response, send_file
 import pandas as pd
+import re
+from flask import render_template_string, session
+from flask_paginate import Pagination, get_page_parameter
 from sqlalchemy import select, inspect,create_engine
 # from dummy import get_info
-# from mapd.Database import Database
+#from mapd.Database import Database
 from mapd.utils import query_database
 
 app = Flask(__name__, static_folder='static')
-
-
+app.secret_key = 'ma'
 
 @app.route('/')
 def home():
     """ renders the template search_form.html """
     #return render_template('search_form.html')
     return render_template('index.html')
-    # return render_template('combi.html')
 
-###############################
 
 @app.route('/test')
 def ss_form():
@@ -27,7 +27,6 @@ def ss_form():
     #return render_template('search_form.html')
     return render_template('combi.html')
 
-#######################################
 
 # search_form.html contains:
 # a form with a text input field for the keyword
@@ -35,18 +34,59 @@ def ss_form():
 # and a submit button that triggers the search_results()
 
 
+# @app.route('/search_results', methods=['GET', 'POST'])
+# def search_results():
+#     if request.method == 'POST':
+#         if 'keyword' in request.form:
+#             keyword = request.form['keyword']
+#             start_date = request.form['start_date']
+#             end_date = request.form['end_date']
+#             results = query_database(keyword, start_date, end_date)  # db_path might be a parameter in get_info() - modify acc.
+#             # Store the keyword and start_date/end_date in the session
+#             session['keyword'] = keyword
+#             session['start_date'] = start_date
+#             session['end_date'] = end_date
+#         else:
+#             # handle the case where the keyword field is missing
+#             return 'The keyword field is missing from the form. Please try again.'
+#         page = request.args.get(get_page_parameter(), type=int, default=1)
+#         per_page = 5
+#         pagination = Pagination(page=page, total=len(results), per_page=per_page)
+#         session['searched_results'] = results
+#         return render_template('combi.html',
+#                                results=results[(page - 1) * per_page:page * per_page],
+#                                pagination=pagination)
+#     else:
+#         # Retrieve the keyword and start_date/end_date from the session
+#         keyword = session.get('keyword', None)
+#         start_date = session.get('start_date', None)
+#         end_date = session.get('end_date', None)
+#         if keyword:
+#             results = query_database(keyword, start_date, end_date)
+#             page = request.args.get(get_page_parameter(), type=int, default=1)
+#             per_page = 5
+#             pagination = Pagination(page=page, total=len(results), per_page=per_page)
+#             session['searched_results'] = results
+#             return render_template('combi.html',
+#                                    results=results[(page - 1) * per_page:page * per_page],
+#                                    pagination=pagination)
+#         else:
+#             return 'No keyword found in the session. Please try again.'
+
 @app.route('/results', methods=['POST'])
 def search_results():
     """ retrieves the keyword, start and end dates from the form. Calls the get_info()
     with these parameters to retrieve relevant abstracts from the DB.
     Renders the template search_results.html with the results passed as a parameter"""
+
     keyword = request.form['keyword']
     start_date = request.form['start_date']
     end_date = request.form['end_date']
-    results = query_database(keyword, start_date, end_date)  # db_path might be a parameter in get_info() - modify acc.
-
+    results = query_database(keyword, start_date, end_date)
+    # if len(results) == 0:
+    #     return render_template('combi.html', message="No results found, try again")
+    # else:
     return render_template('combi.html', results=results)
-
 
 # search_results.html
 # displays the search results in a table format
@@ -56,6 +96,20 @@ def search_results():
 
 
 # search_form.html has a submit button to download file
+# @app.route('/download', methods=['POST'])
+# def download_results():
+#     """ converts the results to a dataframe to generate a CSV file, which
+#     the user can download."""
+#     keyword = request.form['keyword']
+#     results = session.get('searched_results', [])
+#     for result in results:
+#         result['pubmed_id'] = f'=HYPERLINK("https://www.ncbi.nlm.nih.gov/pubmed/{result["pubmed_id"]}","{result["pubmed_id"]}")'
+#         result['abstract_text'] = result['abstract_text'].replace('<mark>', '').replace('</mark>', '')
+#         # result['abstract_text'] = result['abstract'].replace('<mark>'+keyword+'</mark>', keyword)
+#     df = pd.DataFrame(results)
+#     df.to_csv('results.csv', index=False)
+#     return send_file('results.csv', as_attachment=True)
+
 @app.route('/download', methods=['POST'])
 def download_results():
     """ converts the results to a dataframe to generate a CSV file, which
@@ -63,14 +117,12 @@ def download_results():
     keyword = request.form['keyword']
     start_date = request.form['start_date']
     end_date = request.form['end_date']
-    # DB_PATH = 'data/gp2_plab2.db'
-    # CONN_STRING = f"sqlite:///{DB_PATH}"
-    # engine = create_engine(CONN_STRING)
-    # test_session = Session(bind=engine)
     results = query_database(keyword, start_date, end_date)  # modify when task 3 is complete
     for result in results:
-        result['id'] = f'=HYPERLINK("https://www.ncbi.nlm.nih.gov/pubmed/{result["PMID"]}","{result["PMID"]}")'
-        result['abstract_text'] = result['abstract'].replace('<mark>'+keyword+'</mark>', keyword)
+        result['pubmed_id'] = f'=HYPERLINK("https://www.ncbi.nlm.nih.gov/pubmed/{result["pubmed_id"]}","{result["pubmed_id"]}")'
+        # result['abstract'] = result['abstract'].replace('<mark>'+keyword+'</mark>', keyword)
+        #result['abstract_text'] = re.sub("<mark>.*?</mark>", keyword.lower(), result['abstract_text'])
+        result['abstract_text'] = result['abstract_text'].replace('<mark>', '').replace('</mark>', '')
     df = pd.DataFrame(results)
     df.to_csv('results.csv', index=False)
     return send_file('results.csv', as_attachment=True)
@@ -89,6 +141,9 @@ if __name__ == '__main__':
     else:
         # If the FLASK_PORT environment variable is not set, use the default port of 5000
         app.run(debug=True, port=5000, host="0.0.0.0")
+
+
+
 
 
 
--- project_package/mapd/utils.py
+++ project_package/mapd/utils.py
@@ -8,7 +8,8 @@ from mapd.models import Base, Abstract,Entity
 from sqlalchemy import select, inspect, create_engine
 from sqlalchemy.orm import Session
 import re
-DB_PATH = 'E:\Desktop\LSISem3\Plab2\Projects\group project plab2\data\gp2_plab2.db'
+# DB_PATH = 'E:\Desktop\LSISem3\Plab2\Projects\group project plab2\data\gp2_plab2.db'
+DB_PATH = 'D:\ClonedRepoSheet1\group2nlp\data\gp2_plab2.db'
 CONN_STRING = f"sqlite:///{DB_PATH}"
 engine = create_engine(CONN_STRING)
 test_session = Session(bind=engine)
@@ -18,23 +19,75 @@ try:
 except Exception as e:
     print(f"Error connecting to database: {e}")
 
-def query_database(self, keyword: str, start_date=None, end_date=None):
-    """Returns all abstracts in the database that have been tagged with the queried keyword, and were published
-    between the start and end dates (if date parameters are input).
 
-    Returns:
-    A list of dict, each dict represents a row in the table, where keys are the column names"""
+# def query_database(keyword: str, start_date=None, end_date=None):
+#     """Returns all abstracts in the database that have been tagged with the queried keyword, and were published
+#     between the start and end dates (if date parameters are input).
+#
+#     Returns:
+#     A list of dict, each dict represents a row in the table, where keys are the column names"""
+#
+#     query_ = test_session.query(Abstract).outerjoin(Entity, Abstract.id == Entity.abstract_id)
+#     query_ = query_.filter(Entity.entity.like('%' + keyword + '%'))
+#     if start_date and end_date:
+#         query_ = query_.filter(Abstract.date >= start_date, Abstract.date <= end_date)
+#
+#     df = pd.read_sql(query_.statement, query_.session.bind)
+#     # df['date']=
+#     # highlight keyword in the abstract text
+#     df["abstract_text"] = df["abstract_text"].apply(
+#         lambda x: re.sub(f'({keyword})', r'<mark>\1</mark>', x, flags=re.IGNORECASE))
+#     return df.to_dict(orient='records')
+#     #return df.to_dict(orient='records')
 
-    query_ = test_session.query(Abstract).outerjoin(Entity, Abstract.id == Entity.abstract_id)
-    query_ = query_.filter(Entity.labels.like('%' + keyword + '%'))
+def get_abstract_info(pubmed_id: int):
+    engine = create_engine(CONN_STRING)
+    test_session = Session(bind=engine)
+    stmt = select(
+        Abstract.pubmed_id,
+        Abstract.Title,
+        Abstract.date,
+        Abstract.abstract_text,
+        Entity.entity,
+        Entity.labels
+    ).filter_by(pubmed_id=pubmed_id).join(Entity, isouter=True)
+
+    entries = test_session.execute(stmt).fetchall()
+    ent = {}
+    for pubmed_id, title, date, abstract_text, entity, labels in entries:
+        ent[labels] = entity
+    for pubmed_id, title, date, abstract_text, entity, labels in entries:
+        entries_dict = {
+            "pubmed_id": pubmed_id,
+            "Title": title,
+            "date": date.strftime("%Y-%m-%d"),
+            "abstract_text": abstract_text,
+            "entities": ', '.join(ent.values()),
+            "labels": ', '.join(ent.keys())
+        }
+    return entries_dict
+
+def query_database(keyword: str, start_date=None, end_date=None):
+    engine = create_engine(CONN_STRING)
+    test_session = Session(bind=engine)
+    query_ = test_session.query(Abstract.pubmed_id).join(Entity, Abstract.id == Entity.abstract_id)
+    query_ = query_.filter(Entity.entity.like('%' + keyword + '%'))
     if start_date and end_date:
         query_ = query_.filter(Abstract.date >= start_date, Abstract.date <= end_date)
-
-    df = pd.read_sql(query_.statement, query_.session.bind)
-
-    # highlight keyword in the abstract text
-    df["abstract_text"] = df["abstract_text"].apply(
+    pubmed_id_set = set([row[0] for row in query_.all()])
+    pubmed_id_set = list(pubmed_id_set)
+    test_list=[]
+    for pub_id in pubmed_id_set:
+        test_list.append(get_abstract_info(pub_id))
+    df = pd.DataFrame(test_list)
+    if 'abstract_text' in df.columns:
+        df["abstract_text"] = df["abstract_text"].apply(
         lambda x: re.sub(f'({keyword})', r'<mark>\1</mark>', x, flags=re.IGNORECASE))
     return df.to_dict(orient='records')
-# if __name__ == '__main__':
-#     query_database("19","2021-01-01", "2021-12-31")
+
+
+if __name__ == '__main__':
+    # print(query_database("amino acid","2022-01-01", "2022-12-31"))
+    print(query_database("penile"))
+
+
